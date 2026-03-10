a5482e552b90c9427aa2c37feb44a959693bfb16
siddhusingh21467-lab <siddhusingh21467@gmail.com>
2026-03-10 09:08:18 +0530

    UC5 completed

---
+++ UC5.java
@@ -0,0 +1,15 @@
+public class UC5 {
+    public static void main(String[] args) {
+        String[] lines = {
+            " *****   *****  ******  ****  ",
+            "*   *  *   *  *    *  *     ",
+            "*   *  *   *  ******  ***** ",
+            "*   *  *   *  *       *   *",
+            " *****   *****  *       ****  "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
